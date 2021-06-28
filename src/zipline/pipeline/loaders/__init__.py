6dee9d3ecc3d4d6708070ae7874857a06633be75
choiyoungmin <dudals3844@gmail.com>
2021-06-28 17:07:50 +0900

    Add __init__.py KREquity

--- src/zipline/pipeline/loaders/__init__.py
+++ src/zipline/pipeline/loaders/__init__.py
@@ -1,9 +1,11 @@
 from .equity_pricing_loader import (
     EquityPricingLoader,
     USEquityPricingLoader,
+    KREquityPricingLoader
 )
 
 __all__ = [
     "EquityPricingLoader",
     "USEquityPricingLoader",
+    "KREquityPricingLoader"
 ]
